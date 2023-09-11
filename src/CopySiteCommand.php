83a3b7c52b958e0d20c6831760aff03f26b409bf
root <root@ip-208-109-33-188.ip.secureserver.net>
2023-09-11 14:41:04 -0300

    updates

--- src/CopySiteCommand.php
+++ src/CopySiteCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Filesystem\Path;
 
 class CopySiteCommand extends CommandBase{
 
+    use WordPressTrait;
 
     public function getName(){
         return 'copy-site';
@@ -18,36 +19,6 @@ class CopySiteCommand extends CommandBase{
         $this->addArgument('to',InputArgument::REQUIRED,'Destination domain to clone site to');
     }
 
-    function extractConstants($code){
-        preg_match_all("/define\(\s*(.*?)\s*,\s*(.*?)\s*\)/",$code,$matches);
-        $arr = [];
-        $unquote = function($v){
-            return trim(preg_replace("/(^['\"])|(['\"]$)/", "",$v));
-        };
-        foreach($matches[1] as $i => $k){
-            $arr[$unquote($k)] = $unquote($matches[2][$i]);
-        }
-        return $arr;
-    }
-
-    function extractConfig($base_dir, $extract_keys=['DB_NAME','DB_USER','DB_PASSWORD']){
-        if(!file_exists($f = $base_dir."/wp-config.php")){
-            $this->out->writeln("<error>wp-config not found in $base_dir</error>");
-            return self::FAILURE;
-        }
-        $config = file_get_contents($f);
-        $config = $this->extractConstants($config);
-        $result = [];
-        foreach($extract_keys as $k){
-            if(!isset($config[$k])){
-                $this->error("Required key '$k' not found in '$f'");
-                return;
-            } else {
-                $result[$k] = $config[$k];
-            }
-        }
-        return $result;
-    }
 
     protected function exec(){
 
